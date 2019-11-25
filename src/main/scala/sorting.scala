d748b30c8f110526de55d425d217e2978164d937
niranjanw <niranjanw@gmail.com>
2019-11-25 07:06:11 -0500

    first commit

--- src/main/scala/sorting.scala
+++ src/main/scala/sorting.scala
@@ -21,61 +21,62 @@ object sorting {
       merge(mergeSort(left), mergeSort(right))
     }
   }
-    def bubbleSort(xs:Array[Int]) :Array [Int] = {
-      for( i <- 0 until xs.length -1; j <- 0 until xs.length -i -i ) {
 
-        if (xs(j) >  xs (j+1)) {
-          val temp = xs (j)
-          xs (j) = xs (j+1)
-          xs (j+1) = temp
-        }
+  def bubbleSort(xs: Array[Int]): Array[Int] = {
+    for (i <- 0 until xs.length - 1; j <- 0 until xs.length - 1 - i) {
+
+      if (xs(j) > xs(j + 1)) {
+        val temp = xs(j)
+        xs(j) = xs(j + 1)
+        xs(j + 1) = temp
       }
-
-      xs
-
     }
 
-    def isort(xs: List[Int]): List[Int] =
+    xs
+
+  }
+
+  def isort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) Nil
     else insert(xs.head, isort(xs.tail))
 
-    def insert(x: Int, xs: List[Int]): List[Int] =
+  def insert(x: Int, xs: List[Int]): List[Int] =
     if (xs.isEmpty || x <= xs.head) x :: xs
     else xs.head :: insert(x, xs.tail)
 
 
-    def insertNew(x : Int, xs : List[Int]) : List[Int] = {
+  def insertNew(x: Int, xs: List[Int]): List[Int] = {
 
-      xs match{
-        case Nil => List(x)
-        case y :: xs1 =>
-          if(y >= x) x :: xs
-      else y :: insert(x,  xs1)
-      }
+    xs match {
+      case Nil => List(x)
+      case y :: xs1 =>
+        if (y >= x) x :: xs
+        else y :: insert(x, xs1)
+    }
+
+  }
+
+  def inSort(xs: List[Int]): List[Int] = {
+    xs match {
+      case Nil => Nil
+      case x :: xs1 => insertNew(x, isort(xs1))
 
     }
 
-    def inSort(xs : List[Int]) : List[Int] = {
-      xs match{
-        case Nil => Nil
-        case x :: xs1 => insertNew(x, isort(xs1))
 
-      }
+  }
 
-
-   }
-
-
+}
 
   object main extends  App{
 
 
     val arry = Array (2,4,1,6,2,8)
-    print(bubbleSort(arry))
+    print(sorting.bubbleSort(arry).mkString(""))
 
   }
 
 
-}
+
 
 
